2f6f0fed6edd60a7df63b8cb617776edebe458b3
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2018-12-29 22:51:05 +0100

    Added arduino sketch, updated driver.

--- sw/driver.py
+++ sw/driver.py
@@ -5,6 +5,15 @@ import serial
 import time
 
 
+LOG_FILE = "temp.log"
+
+def calc_temp(num):
+	return num*-1.65+808
+
+
+
+log = open(LOG_FILE,"a")
+
 ser = serial.Serial(sys.argv[1],baudrate=115200)
 target = int(sys.argv[2])
 
@@ -13,32 +22,40 @@ for i in range(2):
 
 while True:
 	ser.reset_input_buffer()
-	ser.readline()
+	ser.write(b"k") # keepalive
+	ser.flush()
 	line = ser.readline().strip()[:-1]
+	if "Safety" in line.decode("utf-8"):
+		print("Safety power off, waking...")
+		ser.write(b"k")
+		ser.flush()
+		continue
 	data = dict([part.split(":") for part in line.decode("utf-8").split(";")])
+	data["time"] = time.time()
+	data["temp"] = calc_temp(int(data["TEMP"]))
 	print(data)
+	print(data,file=log)
 	if data["DF"]=="0":
 		ser.write(b"d")
 	if data["L"]=="0":
 		ser.write(b"l")
-	temp = int(data["TEMP"])*-1.65+808
-	print("Temp: {}".format(temp))
-	if temp > target:
+	if data["temp"] > target:
 		print("Turning off")
 		if data["TB"] == "1":
-			ser.write(b"T\n")
+			ser.write(b"T")
 		if data["TS"] == "1":
-			ser.write(b"t\n")
+			ser.write(b"t")
 		if data["BOT"] == "1":
 			ser.write(b"b")
-	if temp < (target - 10):
+	if data["temp"] < (target - 10):
 		print("Turning on")
 		if data["TB"] == "0":
-			ser.write(b"T\n")
+			ser.write(b"T")
 		if data["TS"] == "0":
-			ser.write(b"t\n")
+			ser.write(b"t")
 		if data["BOT"] == "0":
 			ser.write(b"b")
 	ser.flush()
+	log.flush()
 	time.sleep(1)
 
---
+++ sw/oven-control/oven-control.ino
@@ -0,0 +1,140 @@
+#define TOP_SMALL 5
+#define TOP_BIG   4
+#define BOT       8
+#define BACK      9
+#define BACK_FAN  3
+#define DOOR_FAN  6
+#define LIGHT     7
+
+bool s_top_small;
+bool s_top_big;
+bool s_bot;
+bool s_back;
+bool s_back_fan;
+bool s_door_fan;
+bool s_light;
+
+void set_relays(){
+  digitalWrite(TOP_SMALL,s_top_small);
+  digitalWrite(TOP_BIG,s_top_big);
+  digitalWrite(BOT,s_bot);
+  digitalWrite(BACK,s_back);
+  digitalWrite(BACK_FAN,s_back_fan);
+  digitalWrite(DOOR_FAN,s_door_fan);
+  digitalWrite(LIGHT,s_light);
+}
+
+void stop_all(){
+  s_top_small = false;
+  s_top_big = false;
+  s_bot = false;
+  s_back = false;
+  s_back_fan = false;
+  s_door_fan = false;
+  s_light =  false;
+  set_relays();
+}
+
+void print_relays(){
+  Serial.print("TS:");
+  Serial.print(s_top_small);
+  Serial.print(";TB:");
+  Serial.print(s_top_big);  
+  Serial.print(";BOT:");
+  Serial.print(s_bot);  
+  Serial.print(";BACK:");
+  Serial.print(s_back);  
+  Serial.print(";BF:");
+  Serial.print(s_back_fan);  
+  Serial.print(";DF:");
+  Serial.print(s_door_fan);  
+  Serial.print(";L:");
+  Serial.print(s_light);
+  Serial.print(";");    
+}
+
+
+void print_temp(){
+  Serial.print("TEMP:");
+  Serial.print(analogRead(A3));
+  Serial.print(";");
+}
+
+void setup() {
+  pinMode(TOP_SMALL,OUTPUT);
+  pinMode(TOP_BIG,OUTPUT);
+  pinMode(BOT,OUTPUT);
+  pinMode(BACK,OUTPUT);
+  pinMode(BACK_FAN,OUTPUT);
+  pinMode(DOOR_FAN,OUTPUT);
+  pinMode(LIGHT,OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println("Booting...");
+}
+
+int delaycnt;
+
+void loop() {
+  
+  set_relays();
+
+  while (!Serial.available() && delaycnt < 5000){
+    delaycnt++;
+    delay(1);
+  }
+
+  if (delaycnt >= 5000){
+      stop_all();
+      while (!Serial.available()){
+        Serial.println("Safety power off");
+        delay(1000);
+      }
+  }
+
+  delaycnt = 0;
+  
+  print_relays();
+  print_temp();
+  Serial.println();
+
+  if (Serial.available()){
+    char ch;
+    ch = Serial.read();
+    switch(ch){
+      case 't':
+        s_top_small = !s_top_small;
+        break;
+      case 'T':
+        s_top_big = !s_top_big;
+        break;
+      case 'b':
+        s_bot = !s_bot;
+        break;
+      case 'z':
+        if (!s_back){
+          s_back = true;
+          s_back_fan = true;
+        } else {
+          s_back = false;
+          s_back_fan = false;
+        }
+        break;
+      case 'f':
+        s_back_fan = !s_back_fan;
+        if (s_back && !s_back_fan){
+          s_back = false;
+        }
+        break;
+      case 'd':
+        s_door_fan = !s_door_fan;
+        break;
+      case 'l':
+        s_light = !s_light;
+        break;
+      case 'k': //keepalive packet
+        break;
+    }
+  }
+  
+}
